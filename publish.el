4a23fcc962ac5da88b9a35b25605b0088a6aa167
Kostas Tsiligkiris <ktsiligkiris@outlook.com>
2021-01-03 16:17:17 +0200

    Initiated jekyll

--- publish.el
+++ publish.el
@@ -4,19 +4,20 @@
         ("posts"
          :base-directory "~/git/ktsiligkiris.github.io/org/"
          :base-extension "org"
-         :publishing-directory "~/git/ktsiligkiris.github.io/"
+         :publishing-directory "~/git/ktsiligkiris.github.io/_posts"
          :recursive t
          :publishing-function org-html-publish-to-html
          :headline-levels 4   ; Just the default for this project.
-         :auto-sitemap t
+         :html-extensions "html"
+         :body-only t
          )
         ("org-static"
          :base-directory "~/git/ktsiligkiris.github.io/org/"
          :base-extension "css\\|js\\|png\\|jpg\\|git\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory "~/git/ktsiligkiris.github.io/"
+         :publishing-directory "~/git/ktsiligkiris.github.io/assets"
          :recursive t
          :publishing-function org-publish-attachment
          )
-        ("org"
+        ("blog"
          :components ("posts" "org-static"))
         ))
